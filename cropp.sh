fae6c1f251b05f5e1dd02fc21acbd7d09219aa81
suffaro <63808843+suffaro@users.noreply.github.com>
2022-05-22 19:39:22 +0300

    ubuntu 0.0.2

--- cropp.sh
+++ cropp.sh
@@ -56,3 +56,14 @@ yes | sudo apt install git
 
 sudo apt install nodejs
 
+# install brave
+
+yes | sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+yes | sudo apt install brave-browser
