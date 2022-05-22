80709549e0d97972585bed45ab3f5b0488c842d7
suffaro <63808843+suffaro@users.noreply.github.com>
2022-05-22 19:22:00 +0300

    0.0.1 ubuntu

--- cropp.sh
+++ cropp.sh
@@ -33,6 +33,17 @@ yes | sudo apt install python3-pip
 
 yes | sudo apt install vim
 
+# visual studio code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 # install hardware info
 
 yes | sudo apt install hardinfo
